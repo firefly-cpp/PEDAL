6621cf0af1461fb9d41fb681766548b438e36b81
Sebastian Guertl <sebastian.guertl@gmail.com>
2026-03-03 00:01:18 +0100

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,13 +28,8 @@ source_suffix = {
     ".md": "markdown",
 }
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-def setup(app):
-    shutil.copy("../../README.md", "readme.md")
